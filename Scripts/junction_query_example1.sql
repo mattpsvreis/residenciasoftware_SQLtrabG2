9b76f5bcf61d95389b3665afff7abc58df389a6c
Mateus Reis <mateuspsvreis@gmail.com>
2022-04-05 18:57:12 -0300

    Adicionando comentários

--- Scripts/junction_query_example1.sql
+++ Scripts/junction_query_example1.sql
@@ -1,4 +1,4 @@
--- Recupera a lista com todos os produtos e o nome da categoria da onde são.
+-- Recupera a lista com todos os produtos e o nome da categoria da onde são. (6a - 1)
 SELECT p.produto_id, p.produto_nome, c.categoria_nome
 FROM produtos p
 JOIN categorias c
--- Scripts/junction_query_example2.sql
+++ Scripts/junction_query_example2.sql
@@ -1,4 +1,4 @@
--- Código para recuperar os dados de um pedido completo.
+-- Código para recuperar os dados de um pedido completo. (6a - 2)
 SELECT p.pedido_id, p.pedido_data, c.cliente_nome, prod.produto_id, prod.produto_nome, pp.pedidos_produto_qtd AS produto_qtd, 
 prod.produto_valor_unitario AS valor_unitario, pp.pedidos_produto_qtd * prod.produto_valor_unitario AS total_por_produto
 FROM pedidos AS p

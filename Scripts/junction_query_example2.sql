154da657f5b3ef4e8d9be31608ba4b955e94c089
Mateus Reis <mateuspsvreis@gmail.com>
2022-04-05 18:47:23 -0300

    Atualizando junction_query_example2.sql

---
+++ Scripts/junction_query_example2.sql
@@ -0,0 +1,12 @@
+-- Código para recuperar os dados de um pedido completo.
+SELECT p.pedido_id, p.pedido_data, c.cliente_nome, prod.produto_id, prod.produto_nome, pp.pedidos_produto_qtd AS produto_qtd, 
+prod.produto_valor_unitario AS valor_unitario, pp.pedidos_produto_qtd * prod.produto_valor_unitario AS total_por_produto
+FROM pedidos AS p
+INNER JOIN pedidos_produto AS pp 
+	ON pp.pedido_id = p.pedido_id 
+JOIN clientes AS c 
+	ON c.cliente_id = p.cliente_id
+JOIN produtos AS prod 
+	ON prod.produto_id = pp.produto_id
+WHERE p.pedido_id = 1
+GROUP BY prod.produto_id;
